beaddd8a7b509c4473d7122fe2860d0fa4f0d0b0
Rammas <35913846+RammasEchor@users.noreply.github.com>
2020-05-25 14:11:45 -0500

    Prenda.java

---
+++ ControlUniversitario/src/AppTallerCostura/Prenda.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AppTallerCostura;
+
+/**
+ *
+ * @author luis
+ */
+public class Prenda extends Tabla {
+    public Prenda() {
+        super("Prenda");
+        columnasInsert.add("IdConfeccion");
+        columnasInsert.add("CostoTrabajo");
+        columnasInsert.add("CostoMaterial");
+        columnasInsert.add("Finalizado");
+        
+        columnasSelect.add("IdPrenda");
+        columnasSelect.add("IdConfeccion");
+        columnasSelect.add("InfoConfeccion");
+        columnasSelect.add("CostoTrabajo");
+        columnasSelect.add("CostoMaterial");
+        columnasSelect.add("Finalizado");
+                
+        //0 = int, 1 = string, 2 = double o float, 3 = date
+        tipos.add(0);
+        tipos.add(2);
+        tipos.add(2);
+        tipos.add(0);
+        
+        InitializeDeleteQuery();
+        InitializeInsertQuery();
+        InitializeUpdateQuery();
+        
+        selectSQL = "SELECT p.IdPrenda, p.IdConfeccion, CONCAT( cl.Nombre,' ',cl.ApellidoPaterno,' ',cl.ApellidoMaterno,' ',TO_CHAR( c.FechaPedido,'HH12:MI:SS') ) AS InfoConfeccion,"+
+        "CostoTrabajo, CostoMaterial, Finalizado"+
+        "FROM Taller.Prenda AS p "+
+        "INNER JOIN Taller.Confeccion AS c ON p.IdConfeccion = c.IdConfeccion"+
+        "INNER JOIN Taller.Cliente AS cl ON c.IdCliente = cl.IdCliente" ;
+    }
+}
